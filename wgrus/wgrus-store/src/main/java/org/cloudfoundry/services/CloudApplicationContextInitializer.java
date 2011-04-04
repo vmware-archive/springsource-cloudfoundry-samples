e20be462b72b0fbd41ee3b25d7cde5f0097ba350
Mark Fisher <markfisher@vmware.com>
2011-04-04 12:53:03 -0400

    updating wgrus-store to support default/cloud profiles

---
+++ wgrus/wgrus-store/src/main/java/org/cloudfoundry/services/CloudApplicationContextInitializer.java
@@ -0,0 +1,23 @@
+package org.cloudfoundry.services;
+
+import org.cloudfoundry.runtime.env.CloudEnvironment;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author Mark Fisher
+ */
+public class CloudApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+	@Override
+	public void initialize(ConfigurableApplicationContext context) {
+		CloudEnvironment environment = new CloudEnvironment();
+		if (environment.getHost() != null) {
+			context.getEnvironment().setActiveProfiles("cloud");
+		}
+		else {
+			context.getEnvironment().setActiveProfiles("default");
+		}
+	}
+
+}
