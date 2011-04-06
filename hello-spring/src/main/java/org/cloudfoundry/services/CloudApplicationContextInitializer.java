b3fe33f551993b571c36f23fe525e47c008466a1
Mark Fisher <markfisher@vmware.com>
2011-04-06 14:22:02 -0400

    updated CloudApplicationContextInitializer to check for non-null 'instanceInfo' rather than 'cloudApiUri' initially

--- hello-spring/src/main/java/org/cloudfoundry/services/CloudApplicationContextInitializer.java
+++ hello-spring/src/main/java/org/cloudfoundry/services/CloudApplicationContextInitializer.java
@@ -3,22 +3,14 @@ package org.cloudfoundry.services;
 import org.cloudfoundry.runtime.env.CloudEnvironment;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.util.StringUtils;
 
 public class CloudApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
 	@Override
 	public void initialize(ConfigurableApplicationContext applicationContext) {
-		String cloudApiUri = null;
-		try {
-			CloudEnvironment env = new CloudEnvironment();
-			cloudApiUri = env.getCloudApiUri();
-		}
-		catch (Exception e) {
-			// ignore
-		}
-		System.out.println("cloud API: " + cloudApiUri);
-		if (StringUtils.hasText(cloudApiUri)) {
+		CloudEnvironment env = new CloudEnvironment();
+		if (env.getInstanceInfo() != null) {
+			System.out.println("cloud API: " + env.getCloudApiUri());
 			applicationContext.getEnvironment().setActiveProfiles("cloud");
 		}
 		else {
