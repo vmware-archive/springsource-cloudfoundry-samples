74cb0b096741849b2b5748de20225c89d708201c
Dave Syer <dsyer@vmware.com>
2011-04-08 12:14:17 +0100

    Fix WGRUS to deploy in cloud

---
+++ wgrus/wgrus-store/src/main/java/org/wgrus/web/EnvironmentContextListener.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2006-2010 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.wgrus.web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author Dave Syer
+ * 
+ */
+public class EnvironmentContextListener implements ServletContextListener {
+
+	private static Log logger = LogFactory.getLog(EnvironmentContextListener.class);
+
+	public void contextInitialized(ServletContextEvent sce) {
+		if (System.getenv("VMC_APP_VERSION") != null || System.getenv("VCAP_APP_VERSION") != null) {
+			System.setProperty("spring.profiles.active", "cloud");
+			logger.info("Cloud profile activated");
+		} else {
+			System.setProperty("spring.profiles.active", "default");			
+			logger.info("Default profile set");			
+		}
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+	}
+
+}
