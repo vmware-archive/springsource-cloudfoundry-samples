480f9bd7a599c72f482298d6d825cc5d343177bb
Dave Syer <dsyer@vmware.com>
2011-04-11 09:38:13 +0100

    Changes to support migration to CF.com

--- wgrus/wgrus-inventory/src/main/java/org/wgrus/web/EnvironmentContextListener.java
+++ wgrus/wgrus-inventory/src/main/java/org/wgrus/web/EnvironmentContextListener.java
@@ -25,15 +25,30 @@ import org.slf4j.LoggerFactory;
  */
 public class EnvironmentContextListener implements ServletContextListener {
 
-	private static Logger logger = LoggerFactory.getLogger(EnvironmentContextListener.class);
+	private static Logger logger = LoggerFactory
+			.getLogger(EnvironmentContextListener.class);
 
 	public void contextInitialized(ServletContextEvent sce) {
-		if (System.getenv("VMC_APP_VERSION") != null || System.getenv("VCAP_APP_VERSION") != null) {
+
+		if (System.getenv("VMC_APP_VERSION") != null
+				|| System.getenv("VCAP_APP_VERSION") != null) {
+			
 			System.setProperty("spring.profiles.active", "cloud");
-			logger.info("Cloud profile activated");
+			
+			// Extract the base domain from this cloud platform (e.g. cloudfoundry.com)
+			String services = System.getenv("VCAP_APPLICATION");
+			if (services == null) {
+				services = System.getenv("VMC_APP_INSTANCE");
+			}
+			if (services != null) {
+				System.setProperty("BASE_DOMAIN", services.replaceAll(
+						".*wgrus-inventory\\.([a-zA-Z0-9.]*)\\\".*", "$1"));
+			}
+			logger.info("Cloud profile activated with base domain: "+System.getProperty("BASE_DOMAIN"));
+
 		} else {
-			System.setProperty("spring.profiles.active", "default");			
-			logger.info("Default profile set");			
+			System.setProperty("spring.profiles.active", "default");
+			logger.info("Default profile set");
 		}
 	}
 
