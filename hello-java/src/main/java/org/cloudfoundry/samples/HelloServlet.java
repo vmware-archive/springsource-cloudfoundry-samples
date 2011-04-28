c9bad3089cf5808f5c3bd1655a3b7308de4f4ec7
Mark Fisher <markfisher@vmware.com>
2011-04-28 13:31:31 -0400

    added simple hello-java application

---
+++ hello-java/src/main/java/org/cloudfoundry/samples/HelloServlet.java
@@ -0,0 +1,20 @@
+package org.cloudfoundry.samples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		response.setStatus(200);
+		PrintWriter writer = response.getWriter();
+		writer.println("Hello from " + System.getenv("VCAP_APP_HOST") + ":" + System.getenv("VCAP_APP_PORT"));
+		writer.close();
+	}
+}
