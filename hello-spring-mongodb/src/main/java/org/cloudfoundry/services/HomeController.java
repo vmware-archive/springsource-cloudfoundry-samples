c3c7b32fd2ffd3a2a31a0f8c55fccc2caa5cc195
Mark Pollack <mark.pollack@springsource.com>
2011-05-26 14:15:24 -0400

    second attempt to update mongo sample

--- hello-spring-mongodb/src/main/java/org/cloudfoundry/services/HomeController.java
+++ hello-spring-mongodb/src/main/java/org/cloudfoundry/services/HomeController.java
@@ -1,96 +1,128 @@
 package org.cloudfoundry.services;
 
+import static org.springframework.data.document.mongodb.query.Criteria.where;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Random;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.document.mongodb.CollectionCallback;
 import org.springframework.data.document.mongodb.MongoTemplate;
-import org.springframework.data.document.mongodb.query.Criteria;
 import org.springframework.data.document.mongodb.query.Query;
-import org.springframework.data.keyvalue.redis.connection.RedisConnectionFactory;
-import org.springframework.data.keyvalue.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
+import com.mongodb.MongoException;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	@Autowired(required=false) DataSource dataSource;
-	@Autowired(required=false) RedisConnectionFactory redisConnectionFactory;
-	@Autowired(required=false) Mongo mongo;
-	@Autowired(required=false) MongoTemplate template;
-	@Autowired(required=false) @Qualifier(value="serviceProperties") Properties serviceProperties;
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String home(Model model) {
-		List<String> services = new ArrayList<String>();
-		if (dataSource instanceof BasicDataSource) {
-			services.add("Data Source: " + ((BasicDataSource) dataSource).getUrl());
-		}
-		else if (dataSource instanceof SimpleDriverDataSource) {
-			services.add("Data Source: " + ((SimpleDriverDataSource) dataSource).getUrl());
-		}
-		if (redisConnectionFactory != null) {
-			services.add("Redis: " + ((JedisConnectionFactory) redisConnectionFactory).getHostName() + ":" + ((JedisConnectionFactory) redisConnectionFactory).getPort());
-		}
-		if (mongo != null) {
-			services.add("Mongo: " + mongo.getAddress());
-		}
-		
-		
-		Person p = new Person("Mark", 41);
-	  template.save(p);
-	  
-	  List<Person> people = template.find(new Query(Criteria.where("firstName").is("Mark")), Person.class);
-	  
-	  services.add(people.toString());
-	  
-	  
-		//Map envMap = System.getenv();
-		for (Object key : serviceProperties.keySet())
-    {
-        services.add(key + " = " +  serviceProperties.get(key));
+  @Autowired(required = false)
+  Mongo mongo;
+  @Autowired(required = false)
+  MongoTemplate mongoTemplate;
+  @Autowired(required = false)
+  @Qualifier(value = "serviceProperties")
+  Properties serviceProperties;
+
+  /**
+   * Simply selects the home view to render by returning its name.
+   */
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String home(Model model) {
+    List<String> services = new ArrayList<String>();
+    if (mongo != null) {
+      services.add("Mongo: " + mongo.getAddress());
     }
-		
-		model.addAttribute("services", services);
-		String environmentName = (System.getenv("VCAP_APPLICATION") != null) ? "Cloud" : "Local";
-		model.addAttribute("environmentName", environmentName);
-		return "home";
-	}
 
-	@RequestMapping("/env")
-	public void env(HttpServletResponse response) throws IOException {
-		response.setContentType("text/plain");
-		PrintWriter out = response.getWriter();
-		out.println("System Properties:");
-		for (Map.Entry<Object, Object> property : System.getProperties().entrySet()) {
-			out.println(property.getKey() + ": " + property.getValue());
-		}
-		out.println();
-		out.println("System Environment:");
-		for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
-			out.println(envvar.getKey() + ": " + envvar.getValue());
-		}
-	}
+    Random generator = new Random();
+    Person p = new Person("Joe Cloud-" + generator.nextInt(100),
+        generator.nextInt(100));
+    mongoTemplate.save(p);
+
+    List<Person> people = mongoTemplate.find(new Query(where("age").lt(100)), Person.class);
+
+    model.addAttribute("people", people);
+    model.addAttribute("services", services);
+    model.addAttribute("serviceProperties", getServicePropertiesAsList());
+    
+    String environmentName = (System.getenv("VCAP_APPLICATION") != null) ? "Cloud" : "Local";
+    model.addAttribute("environmentName", environmentName);
+    
+    return "home";
+  }
+
+  @RequestMapping("/env")
+  public void env(HttpServletResponse response) throws IOException {
+    response.setContentType("text/plain");
+    PrintWriter out = response.getWriter();
+    out.println("System Properties:");
+    for (Map.Entry<Object, Object> property : System.getProperties().entrySet()) {
+      out.println(property.getKey() + ": " + property.getValue());
+    }
+    out.println();
+    out.println("System Environment:");
+    for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
+      out.println(envvar.getKey() + ": " + envvar.getValue());
+    }
+  }
+
+  @RequestMapping("/service-properties")
+  public void services(HttpServletResponse response) throws IOException {
+    response.setContentType("text/plain");
+    PrintWriter out = response.getWriter();
+    if (serviceProperties != null) {
+      out.println("Cloud Service Properties:");
+      // Map envMap = System.getenv();
+      for (Object key : serviceProperties.keySet()) {
+        out.println(key + ": " + serviceProperties.get(key));
+      }
+    } else {
+      out.println("No Could Service Properties found.  Check configuration file for <cloud:service-properties/> element");
+    }
+    out.println(")<a href=\"/\">Return to previous page.</a>");
+    out.println();
+  }
+  
+  private List<String> getServicePropertiesAsList() {
+    List<String> propList = new ArrayList<String>();
+    if (serviceProperties != null) {
+      for (Object key : serviceProperties.keySet()) {
+        propList.add(key + ": " + serviceProperties.get(key));
+      }
+    }
+    return propList;
+  }
+
+  @RequestMapping("/deleteAll")
+  public void deleteAll(HttpServletResponse response) throws IOException {
+    response.setContentType("text/plain");
+    PrintWriter out = response.getWriter();
+    long count = mongoTemplate.execute(Person.class, new CollectionCallback<Long>() {
+      @Override
+      public Long doInCollection(DBCollection collection) throws MongoException, DataAccessException {
+        return collection.count();
+      }
+    });
+    out.println("Deleted " + count + " entries");
+    mongoTemplate.dropCollection("Person");
+    
+  }
 
 }
--- hello-spring-mongodb/src/main/webapp/WEB-INF/views/home.jsp
+++ hello-spring-mongodb/src/main/webapp/WEB-INF/views/home.jsp
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>Hello Spring</title>
+	<title>Hello Spring MongoDB</title>
 	<link rel="stylesheet" href="static/css/main.css" type="text/css"></link>
 	<link rel="stylesheet" href="static/css/colors.css" type="text/css"></link>
 	<link rel="stylesheet" href="static/css/local.css" type="text/css"></link>
@@ -13,7 +13,7 @@
 		<div id="header">
 			<div id="name-and-company">
 				<div id='site-name'>
-					<a href="" title="Hello Spring ${environmentName}" rel="home">Hello Spring ${environmentName}</a>
+					<a href="" title="Hello Spring MongoDB ${environmentName}" rel="home">Hello Spring MongoDB ${environmentName}</a>
 				</div>
 				<div id='company-name'>
 					<a href="http://www.springsource.com" title="SpringSource">SpringSource Home</a>
@@ -25,82 +25,34 @@
 		<div id="container">
 			<div id="content" class="no-side-nav">
 
-<p>Demonstration of using the 'cloud' namespace to create Spring beans backed by services bound to an application.</p>
+<p>Demonstration of using the 'cloud' namespace to create Spring beans backed by a MongoDB service bound to an application.</p>
 
-<h2>The following services are bound to this application:</h2>
+<p>Every time you reload the page, another Person object is created and stored in MongoDB. The database is then queried and each person added
+to the list you see below.  To reset the list, click <a href="deleteAll">Delete All</a></p>
+
+
+
+<h2>The following people have been stored in the database:</h2>
+<ul>
+	<c:forEach items="${people}" var="people">
+		<li><p>${people}</p></li>	
+	</c:forEach>
+</ul>
+
+<h2>The following services have been bound to this application:</h2>
 <ul>
 	<c:forEach items="${services}" var="service">
 		<li><p>${service}</p></li>	
 	</c:forEach>
 </ul>
 
-<h2>Overview</h2>
+<h2>The following services properties available to the application are available:</h2>
+<ul>
+	<c:forEach items="${serviceProperties}" var="serviceProperties">
+		<li><p>${serviceProperties}</p></li>	
+	</c:forEach>
+</ul>
 
-<h3>Spring Profiles</h3>
-
-<p>This application makes use of Spring 3.1 Profiles. There is an ApplicationContextInitializer
-registered in <code>web.xml</code>:</p>
-
-<pre>
-	&lt;context-param&gt;
-		&lt;param-name&gt;contextInitializerClasses&lt;/param-name&gt;
-		&lt;param-value&gt;org.cloudfoundry.services.CloudApplicationContextInitializer&lt;/param-value&gt;
-	&lt;/context-param&gt;
-</pre>
-
-<p>That initializer activates the "cloud" profile if it recognizes that it's running in a cloud environment.
-Otherwise, it activates the "default" profile.</p>
-
-<p>The configuration contains two different groups of beans that are labeled with the corresponding profile:</p>
-
-<pre>
-	&lt;beans profile="default"&gt;
-		&lt;!-- default bean definitions go here --&gt;
-	&lt;/beans&gt;
-
-	&lt;beans profile="cloud"&gt;
-		&lt;!-- cloud bean definitions go here --&gt;
-	&lt;/beans&gt;
-</pre>
-
-<h3>The "cloud" Namespace</h3>
-
-<p>The beans within the 'cloud' profile section make use of the 'cloud' namespace. That namespace provides
-support for defining beans backed by services:</p>
-
-<pre>
-	&lt;cloud:data-source/&gt;
-
-	&lt;cloud:redis-connection-factory/&gt;
-
-	&lt;cloud:mongo/&gt;
-</pre>
-
-<p>This style works as long as you have only one service of each type bound to the application.</p>
-
-<p>If you want to bind to specific services (perhaps because you have more than one of a kind of service,
-for example two MySql services), you need to specify the <code>service-name</code> attribute as follows:</p>
-
-<pre>
-	&lt;cloud:data-source service-name="inventory-db"/&gt;
-
-	&lt;cloud:data-source service-name="pricing-db"/&gt;
-</pre>
-
-<p>In your Java code, simply add @Autowired dependencies for each bounded service:</p>
-
-<pre>
-	@Autowired DataSource dataSource;
-	@Autowired RedisConnectionFactory redisConnectionFactory;
-	@Autowired Mongo mongo;
-</pre>
-				
-<p>You have access to services without breaking a sweat.
-<br/>
-The service name will be used as the bean id by default, but if you would prefer to use a separate value for that,
-you can specify the 'id' attribute. Regardless of how the id is determined, you can specify the <code>@Qualifier</code> 
-annotation to pick the right bean or even use the XML based configuration. Please check the Spring Framework
-documentation for more details.</p>
 
 			</div>
 		</div>
