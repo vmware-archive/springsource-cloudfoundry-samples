bfd771dd174c18c650bac91d9f52886d43eeb0b1
Peter Ledbrook <peter@cacoethes.co.uk>
2011-07-14 16:03:39 +0100

    Updated project for Grails 2.0.

--- grailstwitter/grails-app/conf/BuildConfig.groovy
+++ grailstwitter/grails-app/conf/BuildConfig.groovy
@@ -12,6 +12,18 @@ grails.project.dependency.resolution = {
         grailsHome()
         grailsCentral()
     }
+    plugins {
+        compile ":spring-security-core:1.1.2"
+        runtime ":blueprint:1.0.2",
+                ":cloud-foundry:1.0",
+                ":cloud-foundry-ui:1.0.1",
+                ":executor:0.2",
+                ":hibernate:2.0.0.BUILD-SNAPSHOT",
+                ":mongodb:1.0.0.M6.1",
+                ":redis:1.0.0.M6",
+                ":searchable:0.6"
+        build ":tomcat:2.0.0.BUILD-SNAPSHOT"
+    }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
     }
