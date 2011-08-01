20d1d61d883921f794510061096c9cf5b088cac6
Mark Fisher <markfisher@vmware.com>
2011-08-01 17:07:17 -0400

    formatting

--- hello-spring-mongodb/src/main/java/org/cloudfoundry/services/HomeController.java
+++ hello-spring-mongodb/src/main/java/org/cloudfoundry/services/HomeController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.mongodb.DBCollection;
-import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 
 /**
@@ -34,96 +33,93 @@ import com.mongodb.MongoException;
 @Controller
 public class HomeController {
 
-  @Autowired(required = false)
-  MongoDbFactory mongoDbFactory;
-  @Autowired(required = false)
-  MongoTemplate mongoTemplate;
-  @Autowired(required = false)
-  @Qualifier(value = "serviceProperties")
-  Properties serviceProperties;
+	@Autowired(required = false)
+	MongoDbFactory mongoDbFactory;
 
-  /**
-   * Simply selects the home view to render by returning its name.
-   */
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String home(Model model) {
-    List<String> services = new ArrayList<String>();
-    if (mongoDbFactory != null) {
-      services.add("Mongo: " + mongoDbFactory.getDb().getMongo().getAddress());
-    }
+	@Autowired(required = false)
+	MongoTemplate mongoTemplate;
 
-    Random generator = new Random();
-    Person p = new Person("Joe Cloud-" + generator.nextInt(100),
-        generator.nextInt(100));
-    mongoTemplate.save(p);
+	@Autowired(required = false)
+	@Qualifier(value = "serviceProperties")
+	Properties serviceProperties;
 
-    List<Person> people = mongoTemplate.find(new Query(where("age").lt(100)), Person.class);
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		List<String> services = new ArrayList<String>();
+		if (mongoDbFactory != null) {
+			services.add("MongoDB: " + mongoDbFactory.getDb().getMongo().getAddress());
+		}
+		Random generator = new Random();
+		Person p = new Person("Joe Cloud-" + generator.nextInt(100), generator.nextInt(100));
+		mongoTemplate.save(p);
+		List<Person> people = mongoTemplate.find(new Query(where("age").lt(100)), Person.class);
 
-    model.addAttribute("people", people);
-    model.addAttribute("services", services);
-    model.addAttribute("serviceProperties", getServicePropertiesAsList());
-    
-    String environmentName = (System.getenv("VCAP_APPLICATION") != null) ? "Cloud" : "Local";
-    model.addAttribute("environmentName", environmentName);
-    
-    return "home";
-  }
+		model.addAttribute("people", people);
+		model.addAttribute("services", services);
+		model.addAttribute("serviceProperties", getServicePropertiesAsList());
 
-  @RequestMapping("/env")
-  public void env(HttpServletResponse response) throws IOException {
-    response.setContentType("text/plain");
-    PrintWriter out = response.getWriter();
-    out.println("System Properties:");
-    for (Map.Entry<Object, Object> property : System.getProperties().entrySet()) {
-      out.println(property.getKey() + ": " + property.getValue());
-    }
-    out.println();
-    out.println("System Environment:");
-    for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
-      out.println(envvar.getKey() + ": " + envvar.getValue());
-    }
-  }
+		String environmentName = (System.getenv("VCAP_APPLICATION") != null) ? "Cloud" : "Local";
+		model.addAttribute("environmentName", environmentName);
+		return "home";
+	}
 
-  @RequestMapping("/service-properties")
-  public void services(HttpServletResponse response) throws IOException {
-    response.setContentType("text/plain");
-    PrintWriter out = response.getWriter();
-    if (serviceProperties != null) {
-      out.println("Cloud Service Properties:");
-      // Map envMap = System.getenv();
-      for (Object key : serviceProperties.keySet()) {
-        out.println(key + ": " + serviceProperties.get(key));
-      }
-    } else {
-      out.println("No Could Service Properties found.  Check configuration file for <cloud:service-properties/> element");
-    }
-    out.println(")<a href=\"/\">Return to previous page.</a>");
-    out.println();
-  }
-  
-  private List<String> getServicePropertiesAsList() {
-    List<String> propList = new ArrayList<String>();
-    if (serviceProperties != null) {
-      for (Object key : serviceProperties.keySet()) {
-        propList.add(key + ": " + serviceProperties.get(key));
-      }
-    }
-    return propList;
-  }
+	@RequestMapping("/env")
+	public void env(HttpServletResponse response) throws IOException {
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		out.println("System Properties:");
+		for (Map.Entry<Object, Object> property : System.getProperties()
+				.entrySet()) {
+			out.println(property.getKey() + ": " + property.getValue());
+		}
+		out.println();
+		out.println("System Environment:");
+		for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
+			out.println(envvar.getKey() + ": " + envvar.getValue());
+		}
+	}
 
-  @RequestMapping("/deleteAll")
-  public void deleteAll(HttpServletResponse response) throws IOException {
-    response.setContentType("text/plain");
-    PrintWriter out = response.getWriter();
-    long count = mongoTemplate.execute(Person.class, new CollectionCallback<Long>() {
-      @Override
-      public Long doInCollection(DBCollection collection) throws MongoException, DataAccessException {
-        return collection.count();
-      }
-    });
-    out.println("Deleted " + count + " entries");
-    mongoTemplate.dropCollection("Person");
-    
-  }
+	@RequestMapping("/service-properties")
+	public void services(HttpServletResponse response) throws IOException {
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		if (serviceProperties != null) {
+			out.println("Cloud Service Properties:");
+			// Map envMap = System.getenv();
+			for (Object key : serviceProperties.keySet()) {
+				out.println(key + ": " + serviceProperties.get(key));
+			}
+		} else {
+			out.println("No Cloud Service Properties found.  Check configuration file for <cloud:service-properties/> element");
+		}
+		out.println(")<a href=\"/\">Return to previous page.</a>");
+		out.println();
+	}
+
+	private List<String> getServicePropertiesAsList() {
+		List<String> propList = new ArrayList<String>();
+		if (serviceProperties != null) {
+			for (Object key : serviceProperties.keySet()) {
+				propList.add(key + ": " + serviceProperties.get(key));
+			}
+		}
+		return propList;
+	}
+
+	@RequestMapping("/deleteAll")
+	public void deleteAll(HttpServletResponse response) throws IOException {
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		long count = mongoTemplate.execute(Person.class,
+				new CollectionCallback<Long>() {
+					@Override
+					public Long doInCollection(DBCollection collection)
+							throws MongoException, DataAccessException {
+						return collection.count();
+					}
+				});
+		out.println("Deleted " + count + " entries");
+		mongoTemplate.dropCollection("Person");
+	}
 
 }
--- hello-spring-mongodb/src/main/java/org/cloudfoundry/services/Person.java
+++ hello-spring-mongodb/src/main/java/org/cloudfoundry/services/Person.java
@@ -1,46 +1,47 @@
 package org.cloudfoundry.services;
 
 public class Person {
-  private String id;
-  private String firstName;
-  private int age;
-  
-  public Person() {
-    
-  }
-  public Person(String firstName, int age) {
-    super();
-    this.firstName = firstName;
-    this.age = age;
-  }
-  
 
-  
-  public String getId() {
-    return id;
-  }
-  public void setId(String id) {
-    this.id = id;
-  }
-  public String getFirstName() {
-    return firstName;
-  }
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-  public int getAge() {
-    return age;
-  }
-  public void setAge(int age) {
-    this.age = age;
-  }
-  @Override
-  public String toString() {
-    return "Person [id=" + id + ", firstName=" + firstName + ", age=" + age
-        + "]";
-  }
+	private String id;
+	private String firstName;
+	private int age;
 
-  
-  
-  
+	public Person() {
+	}
+
+	public Person(String firstName, int age) {
+		super();
+		this.firstName = firstName;
+		this.age = age;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", age=" + age + "]";
+	}
+
 }
