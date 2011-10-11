c96c46a897404b92634b52d4c79ba73774d28c5f
Mark Fisher <markfisher@vmware.com>
2011-10-11 17:19:37 -0400

    simplifications

--- node-spring-flex-chat/flexchat-redis/src/main/java/org/springsource/flexchat/config/ApplicationContext.java
+++ node-spring-flex-chat/flexchat-redis/src/main/java/org/springsource/flexchat/config/ApplicationContext.java
@@ -2,36 +2,15 @@ package org.springsource.flexchat.config;
 
 import java.util.HashMap;
 
-import org.cloudfoundry.runtime.env.CloudEnvironment;
-import org.cloudfoundry.runtime.service.keyvalue.RedisServiceCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.keyvalue.redis.connection.RedisConnectionFactory;
-import org.springframework.data.keyvalue.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.integration.json.JsonInboundMessageMapper;
 import org.springframework.integration.json.JsonOutboundMessageMapper;
 import org.springframework.integration.support.converter.MessageConverter;
 import org.springframework.integration.support.converter.SimpleMessageConverter;
-import org.springframework.util.CollectionUtils;
 
 @Configuration
 public class ApplicationContext {
-	
-	@Bean public RedisConnectionFactory redisConnectionFactory() {
-		if (CollectionUtils.isEmpty(environment().getServices())) {
-			return new JedisConnectionFactory();
-		} else {
-			return redisServiceCreator().createSingletonService().service;
-		}
-	}
-	
-	@Bean CloudEnvironment environment() {
-		return new CloudEnvironment();
-	}
-	
-	@Bean RedisServiceCreator redisServiceCreator() {
-		return new RedisServiceCreator(environment());
-	}
 	
 	@Bean MessageConverter jsonConverter() {
 		SimpleMessageConverter converter = new SimpleMessageConverter();
