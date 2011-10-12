7d5da2d214770c1620cfc79faa5f36e9b1d15c77
Jennifer Hickey <jhickey@vmware.com>
2011-10-12 12:08:01 -0700

    Move springtravel sample to cloudfoundry-samples org
    - Move springtravel to
    https://github.com/cloudfoundry-samples/spring-travel

--- springtravel/clients/android/springtravel/client/src/org/springframework/samples/travel/client/android/domain/Amenity.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.travel.client.android.domain;
-
-import org.simpleframework.xml.Root;
-
-@Root
-public enum Amenity {
-	//todo remember 'smoking' isnt a proper amenity, but well display it as one
-	OCEAN_VIEW, LATE_CHECKOUT, MINIBAR, SMOKING
-}
--- springtravel/server/src/main/java/org/springframework/samples/travel/domain/Amenity.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.samples.travel.domain;
-
-import javax.xml.bind.annotation.XmlEnum;
-
-@XmlEnum
-public enum Amenity {
-	OCEAN_VIEW, LATE_CHECKOUT, MINIBAR
-}
